2f1d1ee8b5b69c3c3c82055152caddfc219d517c
@satyanurhutama1926 <satyanurhutama1926@gmail.com>
2021-11-11 10:58:13 +0700

    commit 3

---
+++ app/src/main/java/com/satya/goesjogja/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.satya.goesjogja.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.satya.goesjogja.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
